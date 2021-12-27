432adfe28df8ef7ea355affe3806fa6fdf3199fd
Erick Guan <fantasticfears@users.noreply.github.com>
2021-12-27 15:57:09 +0100

    set up payments functions

--- src/payments.py
+++ src/payments.py
@@ -2,16 +2,30 @@ from datetime import datetime, timedelta
 from typing import Iterable
 from models import User, ChargeableUser, UserAccess
 from db import Storage, CURRENT_DATE
+import random
 
 
 def should_charge(users: Iterable[User], storage: Storage, checkpoint: datetime) -> Iterable[ChargeableUser]:
     """Returns users that should be charged at the checkpoint time."""
 
-    return []
+    for user in users:
+        transactions = [txn for txn in storage.transactions.values() if txn.user_id == user.id]
+        for txn in transactions:
+            if txn.date + timedelta(days=30) <= checkpoint:
+                yield ChargeableUser(user.id, user.email, user.has_access, txn.product_id)
+
 
 def charge_subscriptions(users: Iterable[User], storage: Storage) -> None:
     """Charge users if possible. Otherwise revoke permissions"""
-    pass
+
+    for user in should_charge(users, storage, CURRENT_DATE):
+        success = random.choice([True, False])
+        if success:
+            # update_access(user, <a transaction id>)
+            pass
+        else:
+            # revoke_access(user, <a product id>)
+            pass
 
 def revoke_access(user: User, product_id: int, storage: Storage) -> None:
     if product_id <= 0: return
--- tests/test_payments.py
+++ tests/test_payments.py
@@ -1,6 +1,6 @@
 from db import Storage
 from models import Transaction
-from payments import revoke_access, update_access
+from payments import revoke_access, should_charge, update_access
 from datetime import datetime, timedelta, timezone
 
 def test_revoke_access__revokes(storage: Storage):
@@ -30,3 +30,35 @@ def test_update_access__creates_record(storage: Storage):
     update_access(storage.users["bob@betao.se"], 7, storage)
 
     assert storage.user_access["1__2"].has_access
+
+
+def test_update_access__creates_record_at_last_day(storage: Storage):
+    storage.transactions.update({
+        7: Transaction(7,  user_id=3, product_id=1, date=datetime(2021, 1, 17, tzinfo=timezone.utc), price=2900)
+    })
+    update_access(storage.users["peter@betao.se"], 7, storage)
+
+    assert storage.user_access["3__1"].has_access
+
+
+def test_should_charge__does_not_charge_within_30_days(storage: Storage):
+    charged = list(should_charge([storage.users["bob@betao.se"]], storage, datetime(2021, 1, 30, tzinfo=timezone.utc)))
+
+    assert len(charged) == 0
+
+def test_should_charge__charge_when_more_than_30_days(storage: Storage):
+    charged = list(should_charge([storage.users["bob@betao.se"]], storage, datetime(2021, 1, 31, tzinfo=timezone.utc)))
+
+    assert len(charged) == 1
+    assert charged[0].id == 1
+    assert charged[0].product_id == 1
+ 
+def test_should_charge__charge_two_products(storage: Storage):
+    charged = list(should_charge([storage.users["boris@betao.se"]], storage, datetime(2021, 1, 14, tzinfo=timezone.utc)))
+
+    assert len(charged) == 2
+    assert charged[0].id == 5
+    assert charged[0].product_id == 1
+
+    assert charged[1].id == 5
+    assert charged[1].product_id == 2
